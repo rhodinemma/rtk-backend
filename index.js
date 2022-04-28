08ecbdfa7bd756a4b7f5a1bf6aa07e894c78b27b
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-04-28 22:04:50 +0300

    setup authentication

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import dotenv from "dotenv"
 import userRouter from "./routes/user.js";
 
 const app = express();
+dotenv.config();
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
