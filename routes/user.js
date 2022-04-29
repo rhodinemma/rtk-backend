907c056765aebb82d07e7cb75f582752ee1b2f41
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-04-30 00:05:19 +0300

    add google signin

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { signin, signup } from "../controllers/user.js";
+import { signin, signup, googleSignIn } from "../controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+router.post("/googleSignIn", googleSignIn);
 
 export default router;
